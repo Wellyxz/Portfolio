b9e83b01ab8442cdbef58e73c52f59828ebc5981
Wellyxyz <walyalfathah@gmail.com>
2025-09-18 14:08:22 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './App.jsx';
 import './styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
